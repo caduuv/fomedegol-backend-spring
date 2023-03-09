609ee0ad721d9ad0fe4aa4c64b01a78ef12c0ff4
Carlos Eduardo Freitas Vidal <ceduardovidalf@gmail.com>
2023-03-08 22:27:11 -0300

    Commit Inicial
    Spring Initializr

---
+++ src/main/java/br/edu/ifce/fomedegolservico/FomeDeGolServicoApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifce.fomedegolservico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FomeDeGolServicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FomeDeGolServicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifce/fomedegolservico/FomeDeGolServicoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifce.fomedegolservico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FomeDeGolServicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
